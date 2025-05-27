fc3fac155b43c9c891f5583cf7e50e3846257fc1
sanji118 <sanjishifa32@gmail.com>
2025-05-27 23:54:52 +0600

    added jwt

--- index.js
+++ index.js
@@ -16,6 +16,39 @@ app.use(express.json());
 
 
 const apartments = require('./apartments.json')
+const coupons = require('./coupons.json')
+
+
+
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).send('Unauthorized');
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(403).send('Forbidden');
+    req.user = decoded; 
+    next();
+  });
+};
+
+const verifyRole = (requiredRole) => {
+  return (req, res, next) => {
+    if (req.user.role !== requiredRole) {
+      return res.status(403).send('Access denied');
+    }
+    next();
+  };
+};
+
+
+app.get('/admin/secure', verifyJWT, verifyRole('admin'), (req, res) => {
+  res.send('Hello Admin!');
+});
+
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.95qfhdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -34,10 +67,18 @@ async function run() {
 
 
     const apartmentCollection = client.db('apartmentsDB').collection('apartments');
-    const couponCollection = client.db('apartmentDB').collection('coupons');
+    const couponCollection = client.db('apartmentsDB').collection('coupons');
     await apartmentCollection.deleteMany({});
     await apartmentCollection.insertMany(apartments);
+    await couponCollection.deleteMany({});
+    await couponCollection.insertMany(coupons);
 
+    //Auth related route
+    app.post('/jwt', async (req, res) => {
+        const user = req.body; // should include email, role, etc.
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
+    });
 
     //Get all data
     app.get('/apartments', async (req, res) => {
