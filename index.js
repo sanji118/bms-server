61eca4d5aef7d3ec35a719c4059c18179e861b42
sanji118 <sanjishifa32@gmail.com>
2025-05-27 17:22:41 +0600

    added mongobd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
-;
+
 app.use(cors({
     origin: ['http://localhost:5173'],
     credentials: true
@@ -15,7 +15,40 @@ app.use(cors({
 app.use(express.json());
 
 
+const apartments = require('./apartments.json')
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.95qfhdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    const apartmentCollection = client.db('apartmentsDB').collection('apartments');
+    await apartmentCollection.deleteMany({});
+    await apartmentCollection.insertMany(apartments);
+
+
+    app.get('/apartments', async(req, res)=>{
+        const newApartment = req.body;
+        const result = await apartmentCollection.insertOne(newApartment);
+        res.send(result);
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
